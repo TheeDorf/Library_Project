ec2ac7c274dcfdc4dc804822a25b347d2d2b4b9d
AJ Dorfman <ajdorfman91@gmail.com>
2023-03-20 19:15:50 -0700

    start js

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
 console.log("Hello World!\n==========\n");
 
 // PROJECT Section
-console.log("PROJECT:\n==========\n");
+class Book {
+    constructor(id, title, author, read){
+        this.id = id;
+        this.title = title;
+        this.author= author;
+        this.read = read;
+    }
+}
+
+class Library {
+    constructor(){
+        this.bookCount = 0;
+        this.books = [];
+    }
+
+    markRead (checkbox, id){
+        for (let i=o; i < this.books.length; i++){
+            if (this.books[i].id){
+                this.books[i].read = true;
+                checkbox.checked = true;
+                checkbox.disabled= true;
+                break;
+            }
+        }
+    }
+}
+
+
+
