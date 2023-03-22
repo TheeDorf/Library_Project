a30ec5e069e57cea4b50a68db57f94acafdeeb8f
AJ Dorfman <ajdorfman91@gmail.com>
2023-03-22 14:16:47 -0700

    fix code

--- app.js
+++ app.js
@@ -2,48 +2,57 @@ console.log("Hello World!\n==========\n");
 
 // PROJECT Section
 class Book {
-    constructor(id, title, author, read){
-        this.id = id;
-        this.title = title;
-        this.author= author;
-        this.read = read;
-    }
+  constructor(id, title, author, read) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.read = read;
+  }
 }
 
 class Library {
-    constructor(){
-        this.bookCount = 0;
-        this.books = [];
-    }
+  constructor() {
+    this.bookCount = 0;
+    this.books = [];
+  }
 
-    markRead (checkbox, id){
-        for (let i=o; i < this.books.length; i++){
-            if (this.books[i].id){
-                this.books[i].read = true;
-                checkbox.checked = true;
-                checkbox.disabled= true;
-                break;
-            }
-        }
+  markRead(checkbox, id) {
+    for (let i = o; i < this.books.length; i++) {
+      if (this.books[i].id) {
+        this.books[i].read = true;
+        checkbox.checked = true;
+        checkbox.disabled = true;
+        break;
+      }
     }
-}
+  }
 
-addBook(title,author,read);{
-    const book = new Book(this.bookCount, title,author,read);
+  addBook(title, author, read) {
+    const book = new Book(this.bookCount, title, author, read);
     this.books.push(book);
-    const table = document.getElementById('booktable');
-    const row = table.insertRow(-1);
+    const table = document.getElementById("table");
+    const row = table.insertRow(1);
     const titleCell = row.insertCell(0);
     const authorCell = row.insertCell(1);
     const readCell = row.insertCell(2);
     titleCell.innerHTML = book.title;
     authorCell.innerHTML = book.author;
 
-    
     this.bookCount++;
   }
-
+}
+let tit = document.querySelector("#title-input")
+let auth = document.querySelector("#author-input")
+let rea = document.querySelector("#read-input")
+auth.value;
+tit.value;
+rea.value;
 
 const myLibrary = new Library();
 const addBooksBtn = document.getElementById("addBooksBtn");
-addBookBtn.addEventListener('click', () => library.addBook());
+const bookForm = document.getElementById("book-form");
+bookForm.addEventListener("submit", (ev) => {
+  console.log("test");
+  ev.preventDefault();
+  myLibrary.addBook(title,author,read);
+});
